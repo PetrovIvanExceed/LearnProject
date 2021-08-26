8476d87ce954397206a40e65d831fd5a48080f36
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-26 16:59:09 +0300

    Task 10

---
+++ Task 10.js
@@ -0,0 +1,48 @@
+// Вернуть true, если все элементы соответствуют условию
+
+let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
+let collectionArr = [
+  {
+    name: 'test',
+    age: 9
+  },
+  {
+    name: 'rest',
+    age: 89
+  },
+  {
+    name: 'west',
+    age: 23
+  },
+  {
+    name: 'test',
+    age: 34
+  }
+]
+
+testFunction = (arr, val, val2) => {
+  let result = false;
+
+  let countAll = lengthFunction(arr);
+  let count = 0;
+  arr.forEach(value => {
+    if(value.name === val && value.age > val2) {
+    count++;
+    }
+  });
+
+  return count === countAll;
+}
+
+lengthFunction = (arr) => {
+  let count = 0;
+
+  arr.forEach((value, index) => {
+    count = index + 1;
+  });
+
+  return count;
+}
+
+const t = testFunction(collectionArr, 'rest', 18);
+console.log(t);
