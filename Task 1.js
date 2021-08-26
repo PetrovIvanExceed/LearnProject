d16b3366ae6d7c607fbe07b2bbc473db5aa82f62
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-26 11:30:47 +0300

    Task 1

--- Task 1.js
+++ Task 1.js
@@ -1,1 +1,15 @@
-bhbhcfghcgh
+let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
+
+lengthFunction = (arr) => {
+  let count = 1;
+  let i = 0;
+  while (arr[i+1]) {
+    i++;
+    count++;
+  }
+
+  return count;
+}
+
+const t = lengthFunction(array);
+console.log(t);
