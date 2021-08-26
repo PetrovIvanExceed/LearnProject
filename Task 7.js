81f1b33b2cea914ef1f18cf3db1498676dd41820
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-26 16:10:52 +0300

    Task 7

---
+++ Task 7.js
@@ -0,0 +1,44 @@
+// В функцию передаем массив и 2 значения. По результату выполнения вывести все
+// элементы массива, которые равны или второму, или третьему параметру.
+
+
+  let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
+let collectionArr = [
+  {
+    name: 'test',
+    age: 9
+  },
+  {
+    name: 'rest',
+    age: 89
+  },
+  {
+    name: 'west',
+    age: 23
+  },
+  {
+    name: 'test',
+    age: 34
+  }
+]
+
+testFunction = (arr, val, val2) => {
+  arr.forEach(value => {
+  if(value > val || value >val2) {
+    console.log(value)
+  }
+  })
+}
+
+lengthFunction = (arr) => {
+  let count = 0;
+
+  arr.forEach((value, index) => {
+    count = index + 1;
+  });
+
+  return count;
+}
+
+const t = testFunction(array, 4, -1);
+console.log(t);
