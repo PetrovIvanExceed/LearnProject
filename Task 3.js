8d239efc1624dafb2537d6ebd7845b29379fc0c0
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-27 12:57:29 +0300

    Task 3

--- Task 3.js
+++ Task 3.js
@@ -1,27 +1,11 @@
 // Вывести количество элементов соответствующих условию
 
-let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
+let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8,
+  244, 2545, 6, 3, 9];
 
 testFunction = (arr, val) => {
-  let newArray = [];
-
-  array.forEach((value, index) => {
-    if(value > val) {
-      newArray.push(value)
-    }
-  });
-
-  return lengthFunction(newArray);
-}
-
-lengthFunction = (arr) => {
-  let count = 0;
-
-  arr.forEach((value, index) => {
-    count = index +1;
-  });
-
-  return count;
+  let newArray = array.filter(value => value > val);
+  return newArray.length;
 }
 
 const t = testFunction(array, 5);
