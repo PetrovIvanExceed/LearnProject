b87388ef88cfd388c3211dd8864e53de83b13315
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-27 13:01:39 +0300

    Task 3

--- Task 3.js
+++ Task 3.js
@@ -6,7 +6,7 @@ let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8,
 
 testFunction = (arr, val) => {
   let newArray = array.filter(value => value > val);
-  return newArray.length;
+  return newArray;
 }
 
 const t = testFunction(array, 5);
