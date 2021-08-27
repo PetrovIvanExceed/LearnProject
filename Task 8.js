4e833f21c23e7a46675587ded9e00b228729892b
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-27 16:40:59 +0300

    Task 8

--- Task 8.js
+++ Task 8.js
@@ -1,10 +1,13 @@
-// Вывод всех объектов, где совпадает сразу несколько полей  значений из массива объектов
+// Вывод всех объектов, где совпадает сразу несколько полей  значений из
+// массива объектов
+// В функцию передаем коллекцию и 2 значения. По результату выполнения вывести
+// все объекты массива, у которых одно свойство ('name' )равно первому параметру
+// или другое свойство ('age') равно второму параметру.
 
-let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
 let collectionArr = [
   {
     name: 'test',
-    age: 9
+    age: 18
   },
   {
     name: 'rest',
@@ -18,28 +21,13 @@ let collectionArr = [
     name: 'test',
     age: 34
   }
-]
+];
 
 testFunction = (arr, val, val2) => {
-  const result = [];
-  arr.forEach(value => {
-    if(value.name === val || value.age > val2) {
-      result.push(value);
-    }
-  });
-
-  return result;
-}
-
-lengthFunction = (arr) => {
-  let count = 0;
-
-  arr.forEach((value, index) => {
-    count = index + 1;
-  });
-
-  return count;
-}
+  let newArray = collectionArr.filter(value =>
+    value.name === val || value.age === val2);
+    return newArray
+};
 
 const t = testFunction(collectionArr, 'rest', 18);
 console.log(t);
