801ec704374a1738b4e419bea48b43402cf73146
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-27 15:41:23 +0300

    Task 7

--- Task 7.js
+++ Task 7.js
@@ -1,41 +1,16 @@
 // Вывод всех элементов, где совпадает сразу несколько значений из массива
+// В функцию передаем массив и 2 значения. По результату выполнения вывести все
+// элементы массива, которые равны или второму, или третьему параметру.
 
-let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244, 2545, 6, 3, 9];
-let collectionArr = [
-  {
-    name: 'test',
-    age: 9
-  },
-  {
-    name: 'rest',
-    age: 89
-  },
-  {
-    name: 'west',
-    age: 23
-  },
-  {
-    name: 'test',
-    age: 34
-  }
-]
+let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244,
+  2545, 6, 3, 9];
 
 testFunction = (arr, val, val2) => {
   arr.forEach(value => {
-  if(value > val || value >val2) {
+  if(value === val || value === val2) {
     console.log(value)
   }
   })
-}
-
-lengthFunction = (arr) => {
-  let count = 0;
-
-  arr.forEach((value, index) => {
-    count = index + 1;
-  });
-
-  return count;
 }
 
 const t = testFunction(array, 4, -1);
