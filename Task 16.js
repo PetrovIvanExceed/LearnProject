f8125dac17bbad1565d7eb54dac811e8f35fccc3
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-08-30 14:42:33 +0300

    Task 16

---
+++ Task 16.js
@@ -0,0 +1,33 @@
+// В функцию подается несколько массивов. Вернуть один массив со всеми элементами.
+
+let array = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244,
+  2545, 6, 3, 9];
+let array1 = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244,
+  2545, 6, 3, 9];
+let array2 = [5, 7, 2, 9, 4, 2, 8, 9, 4, 2, 7, 4, 12, 754, 2345, 2, 8, 244,
+  2545, 6, 3, 9];
+let collectionArr = [
+  {
+    name: 'test',
+    age: 9
+  },
+  {
+    name: 'rest',
+    age: 89
+  },
+  {
+    name: 'west',
+    age: 23
+  },
+  {
+    name: 'test',
+    age: 34
+  }
+];
+
+testFunction = (...arrays) => {
+  return [].concat(...arrays)
+}
+
+const t = testFunction(array, array1, array2);
+console.log(t);
