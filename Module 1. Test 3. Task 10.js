0d05b9bd499f5327138709e0c45a094910897540
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-03 14:06:59 +0300

    Test Task 10

---
+++ Module 1. Test 3. Task 10.js
@@ -0,0 +1,12 @@
+let array = [2, 8, 9, 5, 4, 2, 7, 4, 12, 5, 2, 8, 244, 5];
+const testFunction = (arr, val1, val2) => {
+  let count = arr.reduce((accum, value) => {
+    if(value > val1 && value > val2){
+      accum.push(value)
+    }
+    return accum
+  },[])
+  console.log(count)
+}
+
+testFunction(array, 5, 2)
