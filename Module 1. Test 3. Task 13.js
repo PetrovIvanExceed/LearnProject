69bc60c1941dae965761ae59191046ae0a3f9013
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-06 10:57:29 +0300

    Test Task 13

---
+++ Module 1. Test 3. Task 13.js
@@ -0,0 +1,15 @@
+
+let arr = [12, 42, 63, 56, 122, 17, 117, 25, 645, 722, 964];
+let result1 = [];
+
+for(let i = 0; i < arr.length; i++)
+{
+  if (arr[i] % 2)
+  {
+    result1.push(arr[i]);
+  }
+}
+
+let result2 = arr.filter(n => result1.indexOf(n) === -1);
+
+console.log(result2)
