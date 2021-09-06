eb136862653c4745c38803d35e83324ba59330bc
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-06 18:21:06 +0300

    Test Task 3

--- Module 1. Test 3. Task 3.js
+++
@@ -1,9 +0,0 @@
-let array = [10, 20, 30];
-let count = 0;
-let double = array.map(
-  function doubleNumber( currentValue ) {
-    count += 1;
-    return currentValue * 2;
-  }
-);
-console.log( count );
