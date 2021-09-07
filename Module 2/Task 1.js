a4ea45115aa82fd5ff766e1e95f222ed822d95a1
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-07 14:00:54 +0300

    Module 2. Task 1

--- Module 2/Task 1.js
+++ Module 2/Task 1.js
@@ -2,13 +2,8 @@
 
 let array = [5, -1, 4, 8, -13, 0];
 
-function sortFunc (a, b) {
-  if (a.name > b.name) {
-    return 1;
-  }
-  if (a.name < b.name) {
-    return -1;
-  }
-  // a должно быть равным b
-  return 0;
+function sortFunc(arr) {
+  return arr.sort((a, b) => a - b)
 }
+
+console.log(sortFunc(array))
