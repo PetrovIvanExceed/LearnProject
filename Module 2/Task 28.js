12bd70317318bd482ff5a72281bb8f96892847bc
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-10 17:25:18 +0300

    Module 2. Task 28

---
+++ Module 2/Task 28.js
@@ -0,0 +1,12 @@
+// Из объекта сформировать 2 массива ключи, значения
+ let object = {
+   name: 'test',
+   age: 34,
+   country: 'RF'
+ }
+
+ function testFunc(obj) {
+  return [Object.keys(obj), Object.values(obj)]
+ }
+
+ console.log(testFunc(object))
