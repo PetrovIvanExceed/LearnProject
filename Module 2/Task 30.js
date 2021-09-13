7fde8ad88ede8ac6b872a98140494ba92671fe3a
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-09-13 10:47:50 +0300

    Module 2. Task 30

--- Module 2/Task 30.js
+++ Module 2/Task 30.js
@@ -4,7 +4,7 @@ let array = [5, 7, 2, 9, 5, 6, 3, 1, 8]
 
 function testFunc(arr) {
   let i1 = arr.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
-  let i2 = arr.reduce((iMax, x, i, arr) => x < arr[iMax] ? i : iMax, 0);
+  let i2 = arr.reduce((iMin, x, i, arr) => x < arr[iMin] ? i : iMin, 0);
   let x = arr[i1]
   arr[i1] = arr[i2];
   arr[i2] = x
